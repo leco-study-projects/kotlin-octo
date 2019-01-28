d21befd3b7d1bc943366a1d9d18aecc45adb9af9
leco <lcosta.sp.br@gmail.com>
2019-01-28 17:39:08 -0200

    create project, closes #1

---
+++ src/main/kotlin/com/leco/kotlinocto/KotlinOctoApplication.kt
@@ -0,0 +1,12 @@
+package com.leco.kotlinocto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinOctoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinOctoApplication>(*args)
+}
+
